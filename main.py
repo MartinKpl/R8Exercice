39fbd2856666db4620beaa813f51e6672e6e8825
MartinKpl <martin.kaplan910@gmail.com>
2025-06-14 13:28:00 +0300

    One city displayed in table through Rich library

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import sys, requests
+from rich.console import Console
+from rich.table import Table
 
 if len(sys.argv) < 2:
     print("No city name has been provideded.")
     sys.exit()
 
 city_name = sys.argv[1]
-
-print(f"Weather forcast for city {city_name}")
 
 headers = {
     "User-Agent": "PostmanRuntime/7.43.0",
@@ -31,12 +31,43 @@ if city_coordinates[0] is None or city_coordinates[1] is None:
     print("No coordinates found for the provided city name.")
     sys.exit()
 
-print(f"Coordinates for {city_name}: {city_coordinates}")
-
 response = requests.get(f"https://api.met.no/weatherapi/locationforecast/2.0/compact?lat={city_coordinates[0]}&lon={city_coordinates[1]}", headers=headers)
 
 if response.status_code != 200:
     print("Error fetching data from the forecast API. Reason:", response.reason)
     sys.exit()
 
-print(response.json())
+timeseries = response.json().get("properties", {}).get("timeseries", [])
+
+if len(timeseries) == 0:
+    print("No weather data found for the provided coordinates.")
+    sys.exit()
+
+
+
+table = Table(title=f"Weather Forecast for {city_name.capitalize()}")
+
+columns = ["City name", "Current temperature(Cº/Fº)", "Weather description", "Humidity%", "Wind speed"]
+
+current_weather_details = timeseries[0].get("data", {}).get("instant", {}).get("details", {})
+weather_description = timeseries[0].get("data", {}).get("next_1_hours", {}).get("summary", {}).get("symbol_code", "N/A")
+
+if not current_weather_details:
+    print("No current weather details found.")
+    sys.exit()
+
+row = [
+    city_name.capitalize(),
+    f"{current_weather_details.get('air_temperature', 'N/A')}°C / {current_weather_details.get('air_temperature', 'N/A') * 9/5 + 32:.2f}°F",
+    weather_description.replace('_', ' ').capitalize(),
+    f"{current_weather_details.get('relative_humidity', 'N/A')}%",
+    f"{current_weather_details.get('wind_speed', 'N/A')} m/s"
+]
+
+for column in columns:
+    table.add_column(column)
+
+table.add_row(*row, style='bright_green')
+
+console = Console()
+console.print(table)
